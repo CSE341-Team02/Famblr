63d55a095b0d574b2eb12f5767a3c3bf0da7c756
TheDefallt <57537793+TheDefallt@users.noreply.github.com>
2021-06-15 14:28:00 -0700

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -100,8 +100,60 @@ exports.postLogout = (req, res, next) => {
   });
 };
 
+//Get reset password page
+exports.getReset = (req, res, next) => {
+  let message = req.flash('error');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+  res.render('auth/reset', {
+    path: '/reset',
+    pageTitle: 'Reset Password',
+    errorMessage: message
+  });
+};
 
-//Edit User
+//Reset password
+exports.postReset = (req, res, next) => {
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.log(err);
+      return res.redirect('/reset');
+    }
+    const token = buffer.toString('hex');
+    User.findOne({ email: req.body.email })
+      .then(user => {
+        if (!user) {
+          req.flash('error', 'No account with that email found.');
+          return res.redirect('/reset');
+        }
+        user.resetToken = token;
+        user.resetTokenExpiration = Date.now() + 3600000;
+        return user.save();
+      })
+      .then(result => {
+        res.redirect('/');
+        transporter.sendMail({
+          to: req.body.email,
+          from: 'shop@node-complete.com',
+          subject: 'Password reset',
+          html: `
+            <p>You requested a password reset</p>
+            <p>Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password.</p>
+          `
+        });
+      })
+      .catch(err => {
+        const error = new Error(err);
+        error.httpStatusCode = 500;
+        return next(error);
+      });
+  });
+};
+
+
 //Delete User
 //Show all users??
 
@@ -113,9 +165,6 @@ exports.postLogout = (req, res, next) => {
 //     }
 //   })
 // );
-
-
-
 
 //   User.findOne({ email: email })
 //     .then(user => {
@@ -184,58 +233,6 @@ exports.postLogout = (req, res, next) => {
 //       error.httpStatusCode = 500;
 //       return next(error);
 //     });
-// };
-
-
-// exports.getReset = (req, res, next) => {
-//   let message = req.flash('error');
-//   if (message.length > 0) {
-//     message = message[0];
-//   } else {
-//     message = null;
-//   }
-//   res.render('auth/reset', {
-//     path: '/reset',
-//     pageTitle: 'Reset Password',
-//     errorMessage: message
-//   });
-// };
-
-// exports.postReset = (req, res, next) => {
-//   crypto.randomBytes(32, (err, buffer) => {
-//     if (err) {
-//       console.log(err);
-//       return res.redirect('/reset');
-//     }
-//     const token = buffer.toString('hex');
-//     User.findOne({ email: req.body.email })
-//       .then(user => {
-//         if (!user) {
-//           req.flash('error', 'No account with that email found.');
-//           return res.redirect('/reset');
-//         }
-//         user.resetToken = token;
-//         user.resetTokenExpiration = Date.now() + 3600000;
-//         return user.save();
-//       })
-//       .then(result => {
-//         res.redirect('/');
-//         transporter.sendMail({
-//           to: req.body.email,
-//           from: 'shop@node-complete.com',
-//           subject: 'Password reset',
-//           html: `
-//             <p>You requested a password reset</p>
-//             <p>Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password.</p>
-//           `
-//         });
-//       })
-//       .catch(err => {
-//         const error = new Error(err);
-//         error.httpStatusCode = 500;
-//         return next(error);
-//       });
-//   });
 // };
 
 // exports.getNewPassword = (req, res, next) => {
