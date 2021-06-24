dc48780ae7e9fedc07b4a2a514f2f4fad48d445a
Chandler Childs <chandler@advenna.com>
2021-06-23 22:02:04 -0600

    add account settings page

--- controllers/user.js
+++ controllers/user.js
@@ -7,8 +7,14 @@ const { validationResult } = require("express-validator/check");
 const User = require("../models/user");
 
 exports.getCurrentUser = (req, res, next) => {
+  // This View has access to the user object, because 
+  // it is set as res.locals.user in /index.js
+  res.render("account");
+};
+
+exports.getCurrentUserJSON = (req, res, next) => {
   const user = req.session.user;
-  return res.json(user)
+  return res.json(user);
 };
 
 //Delete User
