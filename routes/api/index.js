41e612d581b10fb8ac6b7ab8c624a4c3ac36f3a2
Chandler Childs <chandler@advenna.com>
2021-07-01 20:55:59 -0600

    add error handlers to api routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,25 @@
 const router = require("express").Router();
 
-router.use('/auth', require('./auth'))
-router.use('/posts', require('./posts'))
-router.use('/users', require('./users'))
-router.use('/comments', require('./comments'))
+router.use("/auth", require("./auth"));
+router.use("/posts", require("./posts"));
+router.use("/users", require("./users"));
+router.use("/comments", require("./comments"));
+
+// 404 Handler
+router.use((req, res, next) => {
+  res.status(404);
+  return res.json({
+    error: "path not found",
+  });
+});
+
+// General Error Handler
+router.use((error, req, res, next) => {
+  console.error(error);
+  res.status(error.httpStatusCode || 500);
+  return res.json({
+    error: error.message || "An unknown server error occured",
+  });
+});
 
 module.exports = router;
